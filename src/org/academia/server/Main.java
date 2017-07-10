dd0653ed3b2c8aee0ae144bf405d139350217563
Manuel Pedro <mbpedro1997@gmail.com>
2017-07-10 15:58:37 +0100

    testing do ne

--- src/org/academia/server/Main.java
+++ src/org/academia/server/Main.java
@@ -2,37 +2,27 @@ package org.academia.server;
 
 import org.academia.sueca.Card;
 import org.academia.sueca.SuecaCards;
-import org.academia.sueca.Suits;
+import org.academia.sueca.Suit;
 
 public class Main {
 
     public static void main(String[] args) {
         Server server = new Server();
         server.init();
-        test();
+        server.start();
     }
 
     private static void test(){
 
         Card[] hand = new Card[7];
 
-<<<<<<< HEAD:src/org/academia/sueca/Main.java
-        hand[0] = new Card(Suit.HEARTS,CardsNumber.SEVEN);
-        hand[1] = new Card(Suit.SPADES,CardsNumber.SIX);
-        hand[2] = new Card(Suit.DIAMONDS,CardsNumber.ACE);
-        hand[3] = new Card(Suit.CLUBS,CardsNumber.KING);
-        hand[4] = new Card(Suit.HEARTS,CardsNumber.JACK);
-        hand[5] = new Card(Suit.HEARTS,CardsNumber.JACK);
-        hand[6] = new Card(Suit.CLUBS,CardsNumber.QUEEN);
-=======
-        hand[0] = new Card(Suits.HEARTS, SuecaCards.SEVEN);
-        hand[1] = new Card(Suits.SPADES, SuecaCards.SIX);
-        hand[2] = new Card(Suits.DIAMONDS, SuecaCards.ACE);
-        hand[3] = new Card(Suits.CLUBS, SuecaCards.KING);
-        hand[4] = new Card(Suits.HEARTS, SuecaCards.JACK);
-        hand[5] = new Card(Suits.HEARTS, SuecaCards.JACK);
-        hand[6] = new Card(Suits.CLUBS, SuecaCards.QUEEN);
->>>>>>> b7d72bff42cb9995bcab7cfac427393a1f34ed11:src/org/academia/server/Main.java
+        hand[0] = new Card(Suit.HEARTS,SuecaCards.SEVEN);
+        hand[1] = new Card(Suit.SPADES, SuecaCards.SIX);
+        hand[2] = new Card(Suit.DIAMONDS,SuecaCards.ACE);
+        hand[3] = new Card(Suit.CLUBS,SuecaCards.KING);
+        hand[4] = new Card(Suit.HEARTS,SuecaCards.JACK);
+        hand[5] = new Card(Suit.HEARTS,SuecaCards.JACK);
+        hand[6] = new Card(Suit.CLUBS,SuecaCards.QUEEN);
 
 
         for (int j = 0; j < 7; j++) {
--- src/org/academia/server/Server.java
+++ src/org/academia/server/Server.java
@@ -3,19 +3,51 @@ package org.academia.server;
 import org.academia.sueca.ClientHandler;
 import org.academia.sueca.Game;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Server {
 
     public static final int PORT = 8080;
-    private ClientHandler[] clients;
-    private Game game;
+    private ServerSocket ss;
+    private List<ClientHandler> clients;
 
     public void init(){
-//TODO:Monte de esterco
+
+        try {
+            ss = new ServerSocket(PORT);
+            clients = new ArrayList<>();
+        } catch (IOException e) {
+            System.err.println("Error on the creation the server! " + e.getMessage());
+            System.exit(1);
+        }
+
     }
 
     public void start(){
 
+
+        while (clients.size() <4){
+
+            try {
+                clients.add(new ClientHandler(ss.accept()));
+                System.out.println(clients.size());
+            } catch (IOException e) {
+                System.err.println(e.getMessage());
+                System.exit(1);
+            }
+        }
+
+        Game game = new Game(clients);
+        game.start();
     }
 
+
+    private class clientHandler{
+
+    }
 
 }
