865edd339c918f8810aea5de973855fae851b0d7
João Rocha <joaorocha@gmail.com>
2017-07-10 15:41:55 +0100

    Implemented the ClientHandler.play() method

--- src/org/academia/sueca/ClientHandler.java
+++ src/org/academia/sueca/ClientHandler.java
@@ -42,7 +42,6 @@ public class ClientHandler {
     }
 
     public void askNick() {
-        //TODO:
         out.println("Enter your nickname:");
 
         try {
@@ -64,6 +63,26 @@ public class ClientHandler {
     public Card play() {
 
         //TODO: check number
+        out.println("Please pick a card number:");
+        int cardPlayer;
+
+        try {
+
+            cardPlayer = Integer.parseInt(in.readLine());
+
+            while (cardPlayer < 0 || cardPlayer > hand.size()){
+
+                out.println("That is not a valid card number. \nPlease insert a number between 1 and " + hand.size());
+                cardPlayer = Integer.parseInt(in.readLine());
+
+            }
+
+        } catch (IOException e) {
+
+            System.err.println(e.getMessage());
+            System.exit(1);
+
+        }
         return null;
     }
 
@@ -77,7 +96,7 @@ public class ClientHandler {
     }
 
     public void addScore(Card[] cards) {
-//TODO: decide on where this logic should be
+    //TODO: decide on where this logic should be
         for (int i = 0; i < cards.length; i++) {
             score += cards[i].getValue();
         }
