0d35d82dde4327053f2b4cfd47617c1f9385dab5
Miguel Ferreira <miguelferreira@MBP-de-Miguel.home>
2017-07-13 22:33:10 +0100

    roleta bets

---
+++ src/org/academia/games/roulette/RouletteBets.java
@@ -0,0 +1,35 @@
+package org.academia.games.roulette;
+
+/**
+ * Created by miguelferreira on 13/07/17.
+ */
+public enum RouletteBets {
+
+/*      Number 0  |
+        Number [1-36]  |
+        BLACK  |
+        RED  |
+        ODD  |
+        EVEN  |
+        UP [1-18] |
+        DOWN [19-36] |
+        1st Dozen [1-12] |
+        2nd Dozen [13-24] |
+        3rd Dozen [25-36]");    */
+
+    ZERO,
+    NUMBER,
+    BLACK,
+    RED,
+    ODD,
+    EVEN,
+    UP,
+    DOWN,
+    DOZEN1,
+    DOZEN2,
+    DOZEN3;
+
+
+   // RouletteBets(RouletteOptions[] options) {
+    //}
+}
--- src/org/academia/games/roulette/RouletteOptions.java
+++ src/org/academia/games/roulette/RouletteOptions.java
@@ -8,57 +8,57 @@ public enum RouletteOptions {
     /**
      * @param value refers to the number value
      * @param color refers to the color RED or BLACK, the zero is the only GREEN
-     * @param oddOrEven if the value is odd #1, if the value is even #2, the zero is #0
-     * @param upOrDown refers to the first #1 or second #2 half of values, the zero is #0
+     * @param oddOrEven if the value is odd, if the value is even, the zero is #0
+     * @param upOrDown refers to the first (DOWN) or second (UP) half of values, the zero is #0
      * @param dozen refers to the first #1, second #2 or third #3 dozen
      */
     //TODO: fazer novo enum para as 10 apostas diferentes
 
-    ZERO(0, "GREEN", 0, "ZERO", 0),
-    ONE(1, "RED", 1, "DOWN", 1),
-    TWO(2, "BLACK", 2, "DOWN", 1),
-    THREE(3, "RED", 1, "DOWN", 1),
-    FOUR(4, "BLACK", 2, "DOWN", 1),
-    FIVE(5, "RED", 1, "DOWN", 1),
-    SIX(6, "BLACK", 2, "DOWN", 1),
-    SEVEN(7, "RED", 1, "DOWN", 1),
-    EIGHT(8, "BLACK", 2, "DOWN", 1),
-    NINE(9, "RED", 1, "DOWN", 1),
-    TEN(10, "BLACK", 2, "DOWN", 1),
-    ELEVEN(11, "BLACK", 1, "DOWN", 1),
-    TWELVE(12, "RED", 2, "DOWN", 1),
-    THIRTEEN(13, "BLACK", 1, "DOWN", 2),
-    FOURTEEN(14, "RED", 2, "DOWN", 2),
-    FIFTEEN(15, "BLACK", 1, "DOWN", 2),
-    SIXTEEN(16, "RED", 2, "DOWN", 2),
-    SEVENTEEN(17, "BLACK", 1, "DOWN", 2),
-    EIGHTEEN(18, "RED", 2, "DOWN", 2),
-    NINETEEN(19, "RED", 1, "UP", 2),
-    TWENTY(20, "BLACK", 2, "UP", 2),
-    TWENTYONE(21, "RED", 1, "UP", 2),
-    TWENTYTWO(22, "BLACK", 2, "UP", 2),
-    TWENTYTHREE(23, "RED", 1, "UP", 2),
-    TWENTYFOUR(24, "BLACK", 2, "UP", 2),
-    TWENTYFIVE(25, "RED", 1, "UP", 3),
-    TWENTYSIX(26, "BLACK", 2, "UP", 3),
-    TWENTYSEVEN(27, "RED", 1, "UP", 3),
-    TWENTYEIGHT(28, "BLACK", 2, "UP", 3),
-    TWENTYNINE(29, "BLACK", 1, "UP", 3),
-    THIRTY(30, "RED", 2, "UP", 3),
-    THIRTYONE(31, "BLACK", 1, "UP", 3),
-    THIRTYTWO(32, "RED", 2, "UP", 3),
-    THIRTYTHREE(33, "BLACK", 1, "UP", 3),
-    THIRTYFOUR(34, "RED", 2, "UP", 3),
-    THIRTYFIVE(35, "BLACK", 1, "UP", 3),
-    THIRTYSIX(36, "RED", 2, "UP", 3);
+    ZERO(0, "GREEN", "ZERO", "ZERO", 0),
+    ONE(1, "RED", "ODD", "DOWN", 1),
+    TWO(2, "BLACK", "EVEN", "DOWN", 1),
+    THREE(3, "RED", "ODD", "DOWN", 1),
+    FOUR(4, "BLACK", "EVEN", "DOWN", 1),
+    FIVE(5, "RED", "ODD", "DOWN", 1),
+    SIX(6, "BLACK", "EVEN", "DOWN", 1),
+    SEVEN(7, "RED", "ODD", "DOWN", 1),
+    EIGHT(8, "BLACK", "EVEN", "DOWN", 1),
+    NINE(9, "RED", "ODD", "DOWN", 1),
+    TEN(10, "BLACK", "EVEN", "DOWN", 1),
+    ELEVEN(11, "BLACK", "ODD", "DOWN", 1),
+    TWELVE(12, "RED", "EVEN", "DOWN", 1),
+    THIRTEEN(13, "BLACK", "ODD", "DOWN", 2),
+    FOURTEEN(14, "RED", "EVEN", "DOWN", 2),
+    FIFTEEN(15, "BLACK", "ODD", "DOWN", 2),
+    SIXTEEN(16, "RED", "EVEN", "DOWN", 2),
+    SEVENTEEN(17, "BLACK", "ODD", "DOWN", 2),
+    EIGHTEEN(18, "RED", "EVEN", "DOWN", 2),
+    NINETEEN(19, "RED", "ODD", "UP", 2),
+    TWENTY(20, "BLACK", "EVEN", "UP", 2),
+    TWENTYONE(21, "RED", "ODD", "UP", 2),
+    TWENTYTWO(22, "BLACK", "EVEN", "UP", 2),
+    TWENTYTHREE(23, "RED", "ODD", "UP", 2),
+    TWENTYFOUR(24, "BLACK", "EVEN", "UP", 2),
+    TWENTYFIVE(25, "RED", "ODD", "UP", 3),
+    TWENTYSIX(26, "BLACK", "EVEN", "UP", 3),
+    TWENTYSEVEN(27, "RED", "ODD", "UP", 3),
+    TWENTYEIGHT(28, "BLACK", "EVEN", "UP", 3),
+    TWENTYNINE(29, "BLACK", "ODD", "UP", 3),
+    THIRTY(30, "RED", "EVEN", "UP", 3),
+    THIRTYONE(31, "BLACK", "ODD", "UP", 3),
+    THIRTYTWO(32, "RED", "EVEN", "UP", 3),
+    THIRTYTHREE(33, "BLACK", "ODD", "UP", 3),
+    THIRTYFOUR(34, "RED", "EVEN", "UP", 3),
+    THIRTYFIVE(35, "BLACK", "ODD", "UP", 3),
+    THIRTYSIX(36, "RED", "EVEN", "UP", 3);
 
     private int value;
     private String color;
-    private int oddOrEven;
+    private String oddOrEven;
     private String upOrDown;
     private int dozen;
 
-    RouletteOptions(int value, String color, int oddOrEven, String upOrDown, int dozen) {
+    RouletteOptions(int value, String color, String oddOrEven, String upOrDown, int dozen) {
 
         this.value = value;
         this.color = color;
@@ -76,7 +76,7 @@ public enum RouletteOptions {
         return color;
     }
 
-    public int getOddOrEven() {
+    public String getOddOrEven() {
         return oddOrEven;
     }
 
@@ -96,7 +96,7 @@ public enum RouletteOptions {
 
         return "NUMBER = " + value +
                 " | COLOR = " + color +
-                " | " + ((oddOrEven == 1) ? "ODD" : "EVEN") +
+                " | " + oddOrEven +
                 " | " + upOrDown +
                 " | DOZEN = " + dozen;
     }
