4e4fdd761a4c5629cdae7b848e5f2775ba3b58a0
João Rocha <joaorocha@gmail.com>
2017-07-16 14:20:34 +0100

    Deleted old TODO

--- src/org/academia/games/president/PresidentPlayer.java
+++ src/org/academia/games/president/PresidentPlayer.java
@@ -9,8 +9,6 @@ import java.util.List;
 
 public class PresidentPlayer extends GameClient {
 
-    //TODO:extend GameClientTest! (to see if its ok)
-
     private List<PCard> hand;
     private String name;
     private boolean passed = false;
